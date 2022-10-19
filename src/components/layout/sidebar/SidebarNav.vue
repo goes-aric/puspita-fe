0498f355002719613a9efcdbd141b68769696b86
Goes Arik <53406260+goes-aric@users.noreply.github.com>
2022-10-19 11:39:01 +0800

    Update SidebarNav.vue
    Updated Sidebar Menu By Jabatan

--- src/components/layout/sidebar/SidebarNav.vue
+++ src/components/layout/sidebar/SidebarNav.vue
@@ -1,93 +1,66 @@
 <template>
   <nav class="px-1 pt-6 overflow-y-auto font-medium text-base sm:px-3 xl:px-5 lg:text-sm pb-10 lg:pt-10 lg:pb-14 h-screen">
     <ul>
-      <li v-for="(section, index) in menu" :key="index" :class="{ 'mt-6' : section.header }">
-        <h5 v-if="section.header !== null" class="nav-header">{{ section.header }}</h5>
-        <span v-if="section.header === null" @click="toggleSidebar">
-          <router-link :to="{ name: section.items[0].url }" class="nav-item">
-            <span class="relative">{{ section.items[0].name }}</span>
-          </router-link>
+      <li class="mt-6">
+        <span @click="toggleSidebar">
+          <router-link :to="{ name: 'dashboard' }" class="nav-item">
+              <span class="relative">Dashboard</span>
+            </router-link>
         </span>
-        <ul v-if="section.header !== null">
-          <li v-for="item in section.items" :key="item.name" @click="toggleSidebar">
-            <router-link :to="{ name: item.url }" class="nav-item">
-              <span class="relative">{{ item.name }}</span>
+        <span v-if="userData.jabatan == 'Admin'" @click="toggleSidebar">
+          <router-link :to="{ name: 'user' }" class="nav-item">
+              <span class="relative">User</span>
+            </router-link>
+        </span>
+        <h5 v-if="userData.jabatan == 'Admin' || userData.jabatan == 'Kepala Pasar'" class="nav-header mt-5">Pendapatan</h5>     
+        <ul v-if="userData.jabatan == 'Admin' || userData.jabatan == 'Kepala Pasar'">
+          <li v-if="userData.jabatan == 'Admin'" @click="toggleSidebar">
+            <router-link :to="{ name: 'kendaraan' }" class="nav-item">
+              <span class="relative">Jenis Kendaraan</span>
+            </router-link>
+          </li>
+          <li @click="toggleSidebar">
+            <router-link :to="{ name: 'aplusan' }" class="nav-item">
+              <span class="relative">Aplusan Pendapatan</span>
+            </router-link>
+          </li>
+          <li @click="toggleSidebar">
+            <router-link :to="{ name: 'rekapitulasi' }" class="nav-item">
+              <span class="relative">Laporan Rekapitulasi</span>
             </router-link>
           </li>
         </ul>
+        <h5 class="nav-header mt-5">Pengeluaran</h5>     
+        <ul>
+          <li v-if="userData.jabatan == 'Admin'" @click="toggleSidebar">
+            <router-link :to="{ name: 'akun' }" class="nav-item">
+              <span class="relative">Akun Pengeluaran</span>
+            </router-link>
+          </li>
+          <li @click="toggleSidebar">
+            <router-link :to="{ name: 'pengeluaran' }" class="nav-item">
+              <span class="relative">Laporan Pengeluaran</span>
+            </router-link>
+          </li>
+        </ul>        
       </li>
     </ul>
   </nav>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 export default {
   name: 'SidebarNav',
-  data(){
-    return {
-      menu: [
-        {
-          header: null,
-          items: [
-            {
-              name: 'Dashboard',
-              isOpen: true,
-              url: 'dashboard'
-            }
-          ]
-        },
-        {
-          header: null,
-          items: [
-            {
-              name: 'User',
-              isOpen: true,
-              url: 'user'
-            }
-          ]
-        },        
-        {
-          header: 'Pendapatan',
-          items: [
-            {
-              name: 'Jenis Kendaraan',
-              isOpen: false,
-              url: 'kendaraan'            
-            },
-            {
-              name: 'Aplusan Pendapatan',
-              isOpen: false,
-              url: 'aplusan'            
-            },
-            {
-              name: 'Laporan Rekapitulasi',
-              isOpen: false,
-              url: 'rekapitulasi'            
-            },                                                                   
-          ]
-        },
-        {
-          header: 'Pengeluaran',
-          items: [
-            {
-              name: 'Akun Pengeluaran',
-              isOpen: false,
-              url: 'akun'
-            },
-            {
-              name: 'Laporan Pengeluaran',
-              isOpen: false,
-              url: 'pengeluaran'
-            },            
-          ]
-        },                          
-      ]
-    }
-  },
   methods: {
     toggleSidebar(){
       this.$store.dispatch('toggleSidebar', !this.isHidden)
     }     
-  }
+  },
+  computed: {
+    ...mapGetters({
+      userData: 'user'
+    })
+  },  
 }
 </script>
